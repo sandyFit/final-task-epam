6610bebf904037d2a9556d65abb6ec840849873c
SandyFit <sandy07r@gmail.com>
2025-07-01 15:12:00 -0500

    first commit

---
+++ src/test/test-data.test.js
@@ -0,0 +1,13 @@
+describe("Page Title", () => {
+    beforeEach(async () => {
+        browser.url("https://www.saucedemo.com/")
+    })
+    /**
+    * @test Validates the page title of the inal Test .
+    */
+    it("Check page title", async () => {
+        const title = await browser.getTitle();
+        console.log(`Page title in the browser: ${title}`)
+        await expect(browser).toHaveTitle("Swag Labs");
+    });
+})
